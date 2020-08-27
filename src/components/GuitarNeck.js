2982c50d22b46d0ac575db45f7f5ca7d07b79d2a
Niklas Almqvist <git@utibu.se>
2020-08-27 20:48:27 +0200

    Fine tuned animations and added text with transposition number to it

--- src/components/GuitarNeck.js
+++ src/components/GuitarNeck.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {motion} from "framer-motion"
 
 import guitarNeck from "../images/guitar_neck3.svg"
@@ -6,15 +6,25 @@ import capo from "../images/capo.svg"
 
 function GuitarNeck(props) {
     const capoStepPercent = 5.9
-    const percentageFromLeft = props.capoPosition <= 0 ? 26.76 + 5.75 * Math.abs(props.capoPosition) : 26.76
+    const percentageFromLeft = props.capoPosition <= 0 ? 25.35 + 5.74 * Math.abs(props.capoPosition) : 25.15
 
     return (
-        <div className="guitar">
+        <div className="guitar" id="guitar">
             <img src={guitarNeck} className="guitarNeck" />
-            <motion.div className="capoContainer" animate={{left: `${percentageFromLeft}%`}}>
-                <h5>{props.capoPosition}</h5>
-                <img src={capo} className="capo" />
+            <motion.div className="capoContainer" 
+                animate={{
+                    left: `${percentageFromLeft}%`
+                }}
+                transition={{
+                    type: "spring", 
+                    stiffness: 500,
+                    damping: 26
+                }}>
+                <img src={capo} className="capo"/>
             </motion.div>
+            <svg viewBox="0 0 56 6" className="capoSvg">
+                <text x="0" y="4" className="capoPosition">Transposition: {props.capoPosition}</text>
+            </svg>
         </div>
     )
 }
